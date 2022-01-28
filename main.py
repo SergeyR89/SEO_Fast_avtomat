9be641796afe77f4b2050e79a2291f0396b436c4
SergeyR89 <dotiandzuzu@mail.ru>
2022-01-28 16:30:32 +0300

    First comit in speed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from time import sleep
 import pickle
 
 
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
